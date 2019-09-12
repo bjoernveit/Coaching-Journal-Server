a604a087b08d177fef3f6cece6d6c53963e8ecb1
Björn Veit <bjoern.veit@arithnea.de>
2019-09-12 18:53:36 +0200

    feat: DBException now with Exception.

--- src/main/java/com/bjoernveit/basketball/coaching/journal/server/Exception/DBException.java
+++ src/main/java/com/bjoernveit/basketball/coaching/journal/server/Exception/DBException.java
@@ -2,4 +2,7 @@ package com.bjoernveit.basketball.coaching.journal.server.Exception;
 
 public class DBException extends Exception {
 
+    public DBException(String msg, Exception e) {
+        super(msg, e);
+    }
 }
